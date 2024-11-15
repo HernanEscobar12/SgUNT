10b5f11da4ccec56947d6d20ae23edc82466ba76
HernanEscobar12 <guidohernanescobar@gmail.com>
2024-11-15 07:08:35 -0300

    Logica para Materia

---
+++ Capa Negocio/AlumnoNegocio.cs
@@ -0,0 +1,45 @@
+﻿using Capa_Datos;
+using Capa_Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocio
+{
+    public class AlumnoNegocio
+    {
+
+        public List<Alumno> ListarAlumnos()
+        {
+            AccesoDatos Datos = new AccesoDatos();  
+            List<Alumno> alumnos = new List<Alumno>();
+
+            try
+            {
+                Datos.SetConsulta("select Id_Alumno , Nombre from ALUMNO");
+                Datos.EjecutarLectura();
+
+                while (Datos.Reader.Read())
+                {
+                    Alumno Aux = new Alumno();
+                    Aux.Id = (int)Datos.Reader["Id_Alumno"];
+                    Aux.Nombre = (string)Datos.Reader["Nombre"];
+
+                    alumnos.Add(Aux);   
+                }
+                return alumnos; 
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+
+    }
+}
