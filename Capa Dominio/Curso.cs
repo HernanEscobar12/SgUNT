9f2801c8ecbb45268c9c4e101338ec6a1ff1c352
HernanEscobar12 <guidohernanescobar@gmail.com>
2024-11-15 07:07:59 -0300

    Agrega Clase Dominio

---
+++ Capa Dominio/Curso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Dominio
+{
+    public class Curso
+    {
+        public int IdCurso {  get; set; }   
+
+        public Alumno Alumno { get; set; }
+
+        public Materia Materia { get; set; }
+
+    }
+}
